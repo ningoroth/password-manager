a90a3a850332122cc04bdf4a24fdcf6d2577ab79
BuildTools <unconfigured@null.spigotmc.org>
2023-02-08 22:34:17 +0100

    GUI, kommentarer, orden i kode

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 ## Imports ##
 import tkinter as tk
-from tkinter import ttk
 import secrets
 import string
 import sqlite3
 import pyperclip
 
-
-## Colors ##
+## Variables ##
+# Colors
 white = "#FFFFFF"
 black = "#000000"
 darkgray = "#222222"
 darkergray = "#363636"
 gray = "#3D3D3D"
+lightgray = "#898989"
 
-LARGEFONT = ("Helvetica", 35)
+title_font = ("Helvetica", 35, "bold")
+standard_font = ("Helvetica", 18)
+
+button_width = 300
+button_height = 50
 
 class TkinterApp(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -43,70 +47,213 @@ class FrontPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, background=gray)
         
-        label = tk.Label(self, text="Password Manager", font=LARGEFONT, foreground=white, background=gray)
-        #label.grid(row=0, column=4, padx=10, pady=10)
-        label.place(relx=0.5, rely=0.1, anchor="n")
+        ## GUI ##
+        # Font Page Title
+        frontPage_title = tk.Label(
+            self, 
+            text = "Password Manager", 
+            font = title_font, 
+            foreground = white, 
+            background = gray
+        )
+        frontPage_title.place(relx=0.5, rely=0.1, anchor="center")
 
-        #style = ttk.Style()
-        #style.configure("W.TButton", font=('calibri', 10, 'bold', 'underline'), background='red')
+        # Generator Button
+        generator_button = tk.Button(
+            self, 
+            text = "Generator", 
+            font = standard_font, 
+            background = darkergray, 
+            foreground = white,
+            activebackground = darkgray,
+            activeforeground = white,
+            command = lambda:controller.showFrame(Generator)
+        )
+        generator_button.place(relx=0.5, rely=0.78, anchor="center", width=button_width, height=button_height)
 
-        button1 = tk.Button(self, text="Generator", font=('Helvetica', 18, 'bold'), bg=darkergray, fg=white, command=lambda:controller.showFrame(Generator))
-        button1.place(relx=0.5, rely=0.78, anchor="center", width=300, height=50)
+        # Add Element Button
+        addElement_button = tk.Button(
+            self, 
+            text = "Add Element", 
+            font = standard_font, 
+            background = darkergray, 
+            foreground = white, 
+            activebackground = darkgray,
+            activeforeground = white,
+            command = lambda:controller.showFrame(AddElement)
+        )
+        addElement_button.place(relx=0.5, rely=0.85, anchor="center", width=button_width, height=button_height)
 
-        button2 = tk.Button(self, text="Add Element", font=('Helvetica', 18, 'bold'), bg=darkergray, fg=white, command=lambda:controller.showFrame(AddElement))
-        button2.place(relx=0.5, rely=0.85, anchor="center", width=300, height=50)
+        # Search Entry Field
+        search_entry = tk.Entry(
+            self, 
+            font = standard_font,
+            background = darkergray,
+            foreground = white
+        )
+        search_entry.place(relx=0.43, rely=0.6, anchor="center", width=button_width-100, height=button_height)
 
-        search_entry = tk.Entry(self, font=("Helvetica", 18))
-        search_entry.place(relx=0.57, rely=0.6, anchor="e", width=200, height=50)
-
-        search_button = tk.Button(self, text="Search", font=('Helvetica', 18, 'bold'), bg=darkergray, fg=white)
-        search_button.place(relx=0.57, rely=0.6, anchor="w", width=100, height=50)
+        # Search Button
+        search_button = tk.Button(
+            self, 
+            text = "Search", 
+            font = standard_font, 
+            background = darkergray, 
+            foreground = white, 
+            activebackground = darkgray,
+            activeforeground = white,
+        )
+        search_button.place(relx=0.64, rely=0.6, anchor="center", width=100, height=50)
 
 
 class Generator(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, background=gray)
 
-        label = ttk.Label(self, text="Generator", font=LARGEFONT, foreground=white, background=gray)
-        label.grid(row=0, column=4, padx=10, pady=10)
+        ## GUI ##
+        # Generator Title
+        generator_title = tk.Label(
+            self, 
+            text = "Generator", 
+            font = title_font, 
+            foreground = white, 
+            background = gray
+        )
+        generator_title.place(relx=0.5, rely=0.1, anchor="center")
 
-        back_button = ttk.Button(self, text="Front Page", command=lambda:controller.showFrame(FrontPage))
-        back_button.grid(row=1, column=1, padx=10, pady=10)
+        # Back Button
+        back_button = tk.Button(
+            self, 
+            text = "Back", 
+            font = standard_font, 
+            background = darkergray, 
+            foreground = white, 
+            activebackground = darkgray,
+            activeforeground = white,
+            command = lambda:controller.showFrame(FrontPage)
+        )
+        back_button.place(x=0, y=0, anchor="nw", width=100, height=50)
 
-        generate_button = tk.Button(self, text="Generate", font=('Helvetica', 18, 'bold'), bg=darkergray, fg=white, command=self.passwordGenerator)
-        generate_button.place(relx=0.5, rely=0.6, anchor="s", width=300, height=50)
+        # Generate Button
+        generate_button = tk.Button(
+            self, 
+            text = "Generate", 
+            font = standard_font, 
+            background = darkergray, 
+            foreground = white,
+            activebackground = darkgray,
+            activeforeground = white, 
+            command = self.passwordGenerator
+        )
+        generate_button.place(relx=0.5, rely=0.6, anchor="s", width=button_width, height=button_height)
 
-        self.thicc_check = tk.BooleanVar()
-        thicc_checkbox = tk.Checkbutton(self, text="A-Z", variable=self.thicc_check)
-        thicc_checkbox.place(relx=0.4, rely=0.5)
+        # Password Text Display
+        self.password_display = tk.Text(
+            self, 
+            font = ("courier", 16), 
+            background = darkergray, 
+            foreground = white, 
+        )
+        self.password_display.place(relx=0.5, rely=0.3, anchor="center", width=500, height=75)
+        self.password_display.configure(state='disabled')
 
-        self.numbers_check = tk.BooleanVar()
-        numbers_checkbox = tk.Checkbutton(self, text="0-9", variable=self.numbers_check)
-        numbers_checkbox.place(relx=0.4, rely=0.6)
+        # Add Password to Element
+        addToElement_button = tk.Button(
+            self,
+            text = "Add to Element", 
+            font = standard_font, 
+            background = darkergray, 
+            foreground = white, 
+            activebackground = darkgray,
+            activeforeground = white,
+        )
+        addToElement_button.place(relx=0.5, rely=0.85, anchor="center", width=button_width, height=button_height)
 
+        # Copy Password Button
+        copy_button = tk.Button(
+            self, 
+            text = "Copy Password", 
+            font = standard_font, 
+            background = darkergray, 
+            foreground = white, 
+            activebackground = darkgray,
+            activeforeground = white,
+            command = self.copyPassword
+        )
+        copy_button.place(relx=0.5, rely=0.78, anchor="center", width=button_width, height=button_height)
+
+        # Uppercase Letters Checkbox
+        self.upper_check = tk.BooleanVar()
+        upper_checkbox = tk.Checkbutton(
+            self, 
+            text = "A-Z", 
+            font=("Helvetiva", 12),
+            background = darkergray, 
+            foreground = white, 
+            activebackground = darkgray,
+            activeforeground = white,
+            selectcolor = darkergray,
+            variable = self.upper_check
+        )
+        upper_checkbox.place(relx=0.32, rely=0.4, anchor="center")
+
+        # Symbols Checkbox
         self.symbols_check = tk.BooleanVar()
-        symbols_checkbox = tk.Checkbutton(self, text="!@#$%^&*", variable=self.symbols_check)
-        symbols_checkbox.place(relx=0.4, rely=0.7)
+        symbols_checkbox = tk.Checkbutton(
+            self, 
+            text = "!@#$%^&*",
+            font=("Helvetiva", 12),
+            background = darkergray, 
+            foreground = white, 
+            activebackground = darkgray,
+            activeforeground = white,
+            selectcolor = darkergray,
+            variable = self.symbols_check
+        )
+        symbols_checkbox.place(relx=0.5, rely=0.4, anchor="center")
 
+        # Numbers Checkbox
+        self.numbers_check = tk.BooleanVar()
+        numbers_checkbox = tk.Checkbutton(
+            self, 
+            text = "0-9",
+            font=("Helvetiva", 12),
+            background = darkergray, 
+            foreground = white, 
+            activebackground = darkgray,
+            activeforeground = white,
+            selectcolor = darkergray,
+            variable = self.numbers_check
+        )
+        numbers_checkbox.place(relx=0.68, rely=0.4, anchor="center")
+
+        # Password Length Scale
         self.length_var = tk.IntVar()
-        length_scale = tk.Scale(self, from_=1, to=100, orient='horizontal', variable=self.length_var)
-        length_scale.place(relx=0.4, rely=0.8)
-
-        self.entry = tk.Text(self)
-        self.entry.place(x=100, y=100, width=200, height=50)
+        length_scale = tk.Scale(
+            self, 
+            background = darkergray, 
+            foreground = white,
+            activebackground= darkgray,
+            troughcolor = darkgray,
+            from_ = 1, 
+            to = 100, 
+            orient = 'horizontal', 
+            variable = self.length_var
+        )
+        length_scale.place(relx=0.5, rely=0.48, anchor="center", width=button_width)
     
     def passwordGenerator(self):
         length = self.length_var.get()
-        thicc_check = self.thicc_check.get()
+        upper_check = self.upper_check.get()
         numbers_check = self.numbers_check.get()
         symbols_check = self.symbols_check.get()
 
-        smol_letters = string.ascii_lowercase
+        lower_letters = string.ascii_lowercase
 
-        if thicc_check == True:
-            thicc_letters = string.ascii_uppercase
+        if upper_check == True:
+            upper_letters = string.ascii_uppercase
         else:
-            thicc_letters = ""
+            upper_letters = ""
 
         if numbers_check == True:
             numbers = string.digits
@@ -118,30 +265,43 @@ class Generator(tk.Frame):
         else:
             symbols = ""
 
-        all = smol_letters + thicc_letters + numbers + symbols
+        temp_password = lower_letters + upper_letters + numbers + symbols
 
-        password = ""
+        self.password = ""
         for i in range(length):
-            password += "".join(secrets.choice(all))
+            self.password += "".join(secrets.choice(temp_password))
 
-        self.entry.configure(state='normal')
-        self.entry.delete(1.0, 'end')
-        self.entry.insert('end', password)
-        self.entry.configure(state='disabled')
-
-        print(password)
-        pyperclip.copy(password)
+        self.password_display.configure(state='normal')
+        self.password_display.delete(1.0, 'end')
+        self.password_display.insert('end', self.password)
+        self.password_display.configure(state='disabled')
+    
+    def copyPassword(self):
+        pyperclip.copy(self.password)
 
 
 class AddElement(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, background=gray)
 
-        label = ttk.Label(self, text="Add Element", font=LARGEFONT, foreground=white, background=gray)
-        label.grid(row=0, column=4, padx=10, pady=10)
+        ## GUI ##
+        # Add Element Title
+        addElement_title = tk.Label(self, text="Add Element", font=title_font, foreground=white, background=gray)
+        addElement_title.place(relx=0.5, rely=0.1, anchor="center")
 
-        button = tk.Button(self, text="Front Page", command=lambda:controller.showFrame(FrontPage))
-        button.grid(row=1, column=1, padx=10, pady=10)
+        # Back Button
+        back_button = tk.Button(
+            self, 
+            text="Back", 
+            font=standard_font, 
+            background=darkergray, 
+            foreground=white,
+            activebackground = darkgray,
+            activeforeground = white,
+            command=lambda:controller.showFrame(FrontPage)
+        )
+        back_button.place(x=0, y=0, anchor="nw", width=100, height=50)
+
 
 screen = TkinterApp()
 screen.geometry("700x750")
